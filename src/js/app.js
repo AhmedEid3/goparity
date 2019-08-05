46640ed21577eac61a0ed8a270d24a6ce6cbed07
AhmedEid3 <ahmed.eid3@outlook.com>
2019-08-05 04:08:04 +0200

    add project files

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // navbar trigger
+  $(".sidenav").sidenav({ edge: "right" });
+
+  // modal trigger
+  $(".modal").modal({
+    inDuration: 250,
+    outDuration: 250
+  });
+});
